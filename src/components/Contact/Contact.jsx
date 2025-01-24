9d5cab5fd0bc9f5d5c3ac41f91887c2077b11fec
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2025-01-24 23:38:35 +0000

    Clean up CSS and start working on Contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,23 @@
-const Contact = () => {};
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <main>
+      <h1 className="global-css-page-title">Contact</h1>
+      <h2>CONTACT ME & AND BOOK YOUR FREE CONSULTATION SESSION</h2>
+      <section className="contact">
+        <summary>
+          <img
+            className="contact-image"
+            src="images/VIPLifeCoachBusinessCard.jpg"
+          />
+        </summary>
+        <aside>
+          <input placeholder="Name" />
+        </aside>
+      </section>
+    </main>
+  );
+};
 
 export default Contact;
